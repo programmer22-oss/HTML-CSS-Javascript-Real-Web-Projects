35effdeff213b8ee47d7120b1c59595da28b75dd
Prathamesh M Vekhande. <176808506+programmer22-oss@users.noreply.github.com>
2025-12-11 10:37:02 +0530

    Add files via upload

---
+++ School Registration/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('registration-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  // Add your form submission logic here
+  alert('Form submitted!');
+});
